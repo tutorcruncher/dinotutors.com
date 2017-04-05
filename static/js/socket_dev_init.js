d55568165d8de9599ccdd32603c979d0620f4e70
tomhamiltonstubber <tomhamiltonstubber@gmail.com>
2017-04-05 10:25:57 +0100

    Add test socket page

---
+++ static/js/socket_dev_init.js
@@ -0,0 +1,11 @@
+socket('9c79f14df986a1ec693c', {
+  mode: 'enquiry',
+  element: '#socket-enquiry'
+});
+
+socket('9c79f14df986a1ec693c');
+
+socket('9c79f14df986a1ec693c', {
+  mode: 'enquiry-modal',
+  element: '#socket-enquiry-modal'
+});
